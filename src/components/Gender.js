965cad0cffd15d4d2a060dff4f81012d4d2ebbae
Kimia Shahbaghi <kshahbaghi@gmail.com>
2022-04-27 02:43:46 +0430

    gender-handler

--- src/components/Gender.js
+++ src/components/Gender.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import GirlImg from "./girl.svg";
 import BoyImg from "./boy.svg";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import "./style.scss";
 
 function NewGender() {
+  const navigate = useNavigate();
+  const [gender, setGender] = useState(undefined);
+  let [check, setCheck] = useState(true);
+  let [selected, setSelected] = useState(false);
+
+  const womanHandler = () => {
+    setGender("woman");
+    setSelected((selected = !selected));
+    if (selected === false) {
+      setGender(undefined);
+    }
+  };
+  console.log(gender, "gender selected");
+  console.log(check, "check");
+  console.log(selected, "selected");
+  const manHandler = () => {
+    setGender("man");
+    setSelected((selected = !selected));
+    if (selected === false) {
+      setGender(undefined);
+    }
+  };
+  const checkHandler = () => {
+    setCheck((check = !check));
+  };
+  const buttonHandler = () => {
+    if (check && selected === true) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  const clickHandler = () => {
+    navigate("/age");
+  };
   return (
     <div className="mycontainer">
       <div className="mycontainer__header">
@@ -15,7 +49,10 @@ function NewGender() {
       <div className="mycontainer__content">
         <p> !لطفا جنسیت خودت رو انتخاب کن </p>
         <div className="mycontainer__content__gender">
-          <div className="flex-child">
+          <div
+            className={gender === "man" ? "flex-child--selected" : "flex-child"}
+            onClick={manHandler}
+          >
             <div class="grid-child">
               <p className="grid-p">آقا</p>
             </div>
@@ -23,7 +60,12 @@ function NewGender() {
               <img src={BoyImg} />
             </div>
           </div>
-          <div className="flex-child">
+          <div
+            className={
+              gender === "woman" ? "flex-child--selected" : "flex-child"
+            }
+            onClick={womanHandler}
+          >
             <div class="grid-child">
               <p className="grid-p">خانم</p>
             </div>
@@ -33,14 +75,25 @@ function NewGender() {
           </div>
         </div>
         <div className="checkbox">
-          <input type="checkbox" className="checkbox__input" checked />
+          <input
+            type="checkbox"
+            className="checkbox__input"
+            checked={check}
+            onClick={checkHandler}
+          />
           <label htmlFor="myCheckBox" className="checkbox__label">
             با قوانین و مقررات موافقم
           </label>
         </div>
       </div>
       <div className="mycontainer__footer">
-        <button className="mycontainer__button">ادامه</button>
+        <button
+          className="mycontainer__button"
+          disabled={buttonHandler()}
+          onClick={clickHandler}
+        >
+          ادامه
+        </button>
       </div>
     </div>
   );
